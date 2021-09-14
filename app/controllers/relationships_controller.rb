8b20092e313502ee57306c93152964bd8906d83e
chiba97 <ktfkyrya@gmail.com>
2021-09-14 06:13:34 +0000

    add

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,13 @@ class RelationshipsController < ApplicationController
   def create
     following = current_user.relationships.build(followed_id: params[:user_id])
     following.save
-    redirect_to request.referrer || root_path
+    redirect_to request.referer || root_path
   end
   
   def destroy
     following = current_user.relationships.find_by(followed_id: params[:user_id])
     following.destroy
-    redirect_to request.referrer || root_path
+    redirect_to request.referer || root_path
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  has_many :relationships, foreign_key: :follower_id
+  has_many :relationships, foreign_key: :follower_id, dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
   
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
   def is_followed_by?(user)
